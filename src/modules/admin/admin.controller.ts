830addd1de739c23de2f5f4d9db6c83591832f01
Soham Mukherjee <soham@noba.com>
2022-05-26 22:09:46 +0530

    Added Api tag for admin apis

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -4,7 +4,7 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { UserID } from '../auth/roles.decorator';
 import { Admin } from '../auth/admin.decorator';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TransactionStatsDTO } from './dto/TransactionStats';
 import { TransactionDTO } from '../transactions/dto/TransactionDTO';
 import { TransactionsFilterDTO } from './dto/TransactionsFilterDTO';
@@ -12,6 +12,7 @@ import { TransactionsFilterDTO } from './dto/TransactionsFilterDTO';
 
 @Admin()
 @Controller("admin/:"+UserID)
+@ApiTags("Admin")
 export class AdminController {
 
   @Inject(WINSTON_MODULE_PROVIDER) 
