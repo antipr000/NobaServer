a7a4c75a17e4e4d0180b90ea6cba326f0f9da2dd
Soham Mukherjee <soham@noba.com>
2022-06-01 16:45:49 +0530

    Updated swagger.json

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Inject, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Inject, HttpStatus, Query, Param } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { UserID } from '../auth/roles.decorator';
 import { Admin } from '../auth/admin.decorator';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { TransactionStatsDTO } from './dto/TransactionStats';
+import { TransactionDTO } from '../transactions/dto/TransactionDTO';
+import { TransactionsFilterDTO } from './dto/TransactionsFilterDTO';
 
 
 @Admin()
@@ -18,10 +21,20 @@ export class AdminController {
 
   }
   
-  @Get("/")
-  @ApiResponse({ status: HttpStatus.OK, description: "Health check for verification service" })
-  async getVerificationStatus(): Promise<string> {
-      return "Hello Noba user. Admin seems to work fine!";
+  @Get("/transaction_metrics")
+  @ApiOperation({ summary: 'Get all transaction metrics for a given partner.' })
+  @ApiResponse({ status: HttpStatus.OK, type: TransactionStatsDTO, description: 'Get transaction statistics' })
+  async getTransactionMetrics(@Param(UserID) userID: string): Promise<TransactionStatsDTO> {
+    return await this.adminService.getTransactionStatus();
+  }
+
+  @Get("/transactions")
+  @ApiOperation({ summary: "Get all transactions filtered by the specified date range" })
+  @ApiResponse({ status: HttpStatus.OK, type: [TransactionDTO] })
+  async getAllTransactions(
+    @Param(UserID)userID: string, 
+    @Query()filterQuery: TransactionsFilterDTO): Promise<TransactionDTO[]> {
+    return await this.adminService.getAllTransactions(filterQuery.startDate, filterQuery.endDate);
   }
 
 }
