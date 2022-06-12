76a8d186649a126df66998fe423e87e8870352b8
Soham Mukherjee <soham@noba.com>
2022-06-12 18:08:39 +0530

    Removed irrelevant prettier rule from eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,8 +27,6 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    // "prettier/prettier": ["error", { "endOfLine": "auto" }]
-    "prettier/prettier": 0,
     quotes: ["error", "double"],
     // we want to force semicolons
     semi: ["error", "always"],
