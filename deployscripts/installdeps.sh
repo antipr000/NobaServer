4b7547913b0a901ad9f1ffff8d08c8f107ed37f9
Justin-noba <justin@noba.com>
2022-06-22 09:59:28 -0700

    Updating node version on server to 16.15.x

--- deployscripts/installdeps.sh
+++ deployscripts/installdeps.sh
@@ -12,7 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 14.15
+nvm install 16.15
 npm install -g pm2
 pm2 install pm2-logrotate
 
