b46a8459f100848f4ac57fb12422ad29952ba1de
Justin-noba <justin@noba.com>
2022-06-27 12:25:50 -0700

    Prettier fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -30,7 +30,11 @@ export class AppController {
         // Exclude XRP
         if (liq === "Yes" && symbol !== "XRP") {
           // TODO: Move this path to config
-          results.push({ name: `${name}`, ticker: `${symbol}`, iconPath: `https://dj61eezhizi5l.cloudfront.net/assets/images/currency-logos/crypto/${symbol.toLowerCase()}.svg` });
+          results.push({
+            name: `${name}`,
+            ticker: `${symbol}`,
+            iconPath: `https://dj61eezhizi5l.cloudfront.net/assets/images/currency-logos/crypto/${symbol.toLowerCase()}.svg`,
+          });
         }
       })
       .on("end", () => {
