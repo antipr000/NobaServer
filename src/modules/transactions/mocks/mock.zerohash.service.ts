c5890e41e3f45499b6fb1614facd069141895336
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2022-07-22 00:19:40 +0530

    Added mocks for zerohash & exchangerate

---
+++ src/modules/transactions/mocks/mock.zerohash.service.ts
@@ -0,0 +1,21 @@
+import { anyString, anything, mock, when } from "ts-mockito";
+import { ZeroHashService } from "../zerohash.service";
+
+export function getMockZerohashServiceWithDefaults(): ZeroHashService {
+  const mockZerohashService = mock(ZeroHashService);
+
+  when(mockZerohashService.createParticipant(anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.estimateNetworkFee(anything(), anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.executeQuote(anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getAccounts()).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getAllParticipants()).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getParticipant(anyString())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getPrice(anything(), anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getTrade(anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.getWithdrawal(anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.makeRequest(anything(), anything(), anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.requestQuote(anyString(), anyString(), anything(), anything())).thenReject(new Error("Method not implemented"));
+  when(mockZerohashService.requestTrade(anything())).thenReject(new Error("Method not implemented"));
+
+  return mockZerohashService;
+}
