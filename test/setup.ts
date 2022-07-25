9eaae3224102f68cfeea29584eed8eb1d16d2824
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2022-07-26 00:27:58 +0530

    Mocked consumers & producerds in e2e

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,63 @@
 import { join } from "path";
 
+class MockSqsProducer {
+  public static producedMessages: Array<any> = [];
+
+  static reset() {
+    MockSqsProducer.producedMessages = [];
+  }
+
+  constructor(public readonly constructedWith: any) {
+    MockSqsProducer.producedMessages = [];
+  }
+
+  send(message: any) {
+    MockSqsProducer.producedMessages.push(message);
+  }
+}
+
+class MockSqsConsumer {
+  public static intializers: Array<any> = [];
+  public static onCalls: Array<any> = [];
+  public static startCallsCount: number = 0;
+
+  static reset() {
+    MockSqsConsumer.intializers = [];
+    MockSqsConsumer.onCalls = [];
+    MockSqsConsumer.startCallsCount = 0;
+  }
+
+  constructor() { }
+
+  static create(initializer: any) {
+    MockSqsConsumer.intializers.push(initializer);
+    return new MockSqsConsumer();
+  }
+
+  on(type: string, callback) {
+    MockSqsConsumer.onCalls.push({
+      type: type,
+      callback: callback,
+    });
+  }
+
+  start() {
+    MockSqsConsumer.startCallsCount++;
+  }
+}
+
+jest.mock("sqs-producer", () => {
+  return {
+    Producer: MockSqsProducer,
+  };
+});
+
+jest.mock("sqs-consumer", () => {
+  return {
+    Consumer: MockSqsConsumer,
+  };
+});
+
 export const setUp = () => {
   const port = 9000 + Math.floor(Math.random() * 1000);
 
