7166348ea5301ae0bcbfd3b89dbc5ef0680889a7
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2022-08-03 22:27:37 +0530

    removed the unused imports

--- src/modules/transactions/transaction.module.ts
+++ src/modules/transactions/transaction.module.ts
@@ -3,7 +3,6 @@ import { InfraProvidersModule } from "../../infraproviders/infra.module";
 import { CommonModule } from "../common/common.module";
 import { ConsumerModule } from "../consumer/consumer.module";
 import { VerificationModule } from "../verification/verification.module";
-import { PendingTransactionDBPollerService } from "./crons/PendingTransactionsDBPoller.cron";
 import { LimitsService } from "./limits.service";
 import { MongoDBTransactionRepo } from "./repo/MongoDBTransactionRepo";
 import { TransactionController } from "./transaction.controller";
@@ -11,16 +10,11 @@ import { TransactionService } from "./transaction.service";
 import { ZeroHashService } from "./zerohash.service";
 import { AppService } from "../../app.service";
 import { AsyncTransactionProcessorModule } from "./queueprocessors/processors.module";
-import { SqsClient } from "./queueprocessors/sqs.client";
-import { TransactionPollerService } from "./crons/transaction.poller.cron";
 
 @Module({
   imports: [InfraProvidersModule, CommonModule, ConsumerModule, VerificationModule, AsyncTransactionProcessorModule],
   controllers: [TransactionController],
   providers: [
-    SqsClient,
-    // PendingTransactionDBPollerService,
-    TransactionPollerService,
     TransactionService,
     LimitsService,
     ZeroHashService,
