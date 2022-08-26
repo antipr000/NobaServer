7d8d27a83cc58dd4bc36ae6dd8360abb544c46a8
Justin-noba <justin@noba.com>
2022-08-26 10:42:45 -0700

    Fixed mocks

--- src/modules/auth/mocks/mock.user.auth.service.ts
+++ src/modules/auth/mocks/mock.user.auth.service.ts
@@ -1,10 +1,10 @@
-import { anyString, mock, when } from "ts-mockito";
+import { anyNumber, anyString, mock, when } from "ts-mockito";
 import { UserAuthService } from "../user.auth.service";
 
 export const getMockUserAuthServiceWithDefaults = () => {
   const mockUserAuthService: UserAuthService = mock(UserAuthService);
 
-  when(mockUserAuthService.validateAndGetUserId(anyString(), anyString(), anyString())).thenReject(
+  when(mockUserAuthService.validateAndGetUserId(anyString(), anyNumber(), anyString(), anyString())).thenReject(
     new Error("Not implemented!"),
   );
   when(mockUserAuthService.generateAccessToken(anyString(), anyString())).thenReject(new Error("Not implemented!"));
