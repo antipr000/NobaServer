709e596bf18c9bdc938d2de5d4eac1b65bdf2199
Justin Ashworth <105762120+Justin-noba@users.noreply.github.com>
2022-09-13 13:12:46 -0700

    Add unit tests for app controller #557 (#558)

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { CurrencyService } from "./modules/common/currency.service";
-import { CurrencyDTO } from "./modules/common/dto/CurrencyDTO";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly currencyService: CurrencyService) {}
-
-  async getSupportedCryptocurrencies(): Promise<Array<CurrencyDTO>> {
-    // TODO(#235): Pull from database post-MVP
-    return this.currencyService.getSupportedCryptocurrencies();
-  }
-
-  async getSupportedFiatCurrencies(): Promise<CurrencyDTO[]> {
-    // TODO(#235): Pull from database post-MVP
-    return this.currencyService.getSupportedFiatCurrencies();
-  }
+  constructor() {}
 }
