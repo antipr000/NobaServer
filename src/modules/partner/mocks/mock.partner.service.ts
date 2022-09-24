92042b05e4befdc47755970d353d8a1ab5570a37
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2022-09-24 23:18:47 +0530

    Added partner configs in Create request of service

--- src/modules/partner/mocks/mock.partner.service.ts
+++ src/modules/partner/mocks/mock.partner.service.ts
@@ -4,7 +4,7 @@ import { PartnerService } from "../partner.service";
 export const getMockPartnerServiceWithDefaults = () => {
   const mockPartnerService: PartnerService = mock(PartnerService);
 
-  when(mockPartnerService.createPartner(anyString())).thenReject(new Error("Not implemented!"));
+  when(mockPartnerService.createPartner(anything())).thenReject(new Error("Not implemented!"));
   when(mockPartnerService.getPartner(anyString())).thenReject(new Error("Not implemented!"));
   when(mockPartnerService.getPartnerFromApiKey(anyString())).thenReject(new Error("Not implemented!"));
   when(mockPartnerService.updatePartner(anyString(), anything())).thenReject(new Error("Not implemented!"));
