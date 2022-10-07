5af8791d0e2dd12c64b9695c311830f818c3990d
Ankit Gaur <ankit@mcbkair.local>
2022-10-07 19:31:41 +0530

    add nest like formatting in our code itself as nest-winston has bug  https://github.com/gremo/nest-winston/pull/582/files

--- src/core/utils/WinstonModule.ts
+++ src/core/utils/WinstonModule.ts
@@ -1,8 +1,11 @@
 import winston from "winston";
-import { utilities as nestWinstonModuleUtilities, WinstonModule } from "nest-winston";
-import { SERVER_LOG_FILE_PATH } from "../../config/ConfigurationUtils";
+import { WinstonModule } from "nest-winston";
+import { SERVER_LOG_FILE_PATH, isLocalDevEnvironment } from "../../config/ConfigurationUtils";
 import { CustomConfigService } from "./AppConfigModule";
-import { isLocalDevEnvironment } from "../../config/ConfigurationUtils";
+
+import { Format } from "logform";
+import { inspect } from "util";
+import safeStringify from "fast-safe-stringify";
 
 export function getWinstonModule() {
   return WinstonModule.forRootAsync({
@@ -17,23 +20,79 @@ export function getWinstonModule() {
         winston.format.timestamp({
           format: () => new Date().toISOString() + " ",
         }),
-        nestWinstonModuleUtilities.format.nestLike("NobaServer", { colors: isLocalDevEnvironment() }),
+        // nestWinstonModuleUtilities.format.nestLike("NobaServer", { colors: isLocalDevEnvironment() }), //TODO https://github.com/gremo/nest-winston/pull/582/files
+        nestLike("NobaServer", { colors: isLocalDevEnvironment(), prettyPrint: false }),
       );
 
       return {
         transports: [
-          new winston.transports.File({
-            filename: logFilePath,
-            level: "debug",
-          }),
           new winston.transports.Console({
             format: winstonFormat,
+            level: process.env["WINSTON_LOG_LEVEL"] ?? "info",
           }),
         ],
       };
     },
   });
 }
+
+// this below can be removed once this pull request is merged https://github.com/gremo/nest-winston/pull/582/files and we have updated version of nest-winston
+const clc = {
+  bold: (text: string) => `\x1B[1m${text}\x1B[0m`,
+  green: (text: string) => `\x1B[32m${text}\x1B[39m`,
+  yellow: (text: string) => `\x1B[33m${text}\x1B[39m`,
+  red: (text: string) => `\x1B[31m${text}\x1B[39m`,
+  magentaBright: (text: string) => `\x1B[95m${text}\x1B[39m`,
+  cyanBright: (text: string) => `\x1B[96m${text}\x1B[39m`,
+};
+
+const nestLikeColorScheme: Record<string, (text: string) => string> = {
+  info: clc.green,
+  error: clc.red,
+  warn: clc.yellow,
+  debug: clc.magentaBright,
+  verbose: clc.cyanBright,
+};
+
+const nestLike = (
+  appName = "NestWinston",
+  options = {
+    colors: !process.env.NO_COLOR,
+    prettyPrint: false,
+  },
+): Format =>
+  winston.format.printf(({ context, level, timestamp, message, ms, ...meta }) => {
+    if ("undefined" !== typeof timestamp) {
+      // Only format the timestamp to a locale representation if it's ISO 8601 format. Any format
+      // that is not a valid date string will throw, just ignore it (it will be printed as-is).
+      try {
+        if (timestamp === new Date(timestamp).toISOString()) {
+          timestamp = new Date(timestamp).toLocaleString();
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-empty
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    const color = (options.colors && nestLikeColorScheme[level]) || ((text: string): string => text);
+    const yellow = options.colors ? clc.yellow : (text: string): string => text;
+
+    const stringifiedMeta = safeStringify(meta);
+    const formattedMeta = options.prettyPrint
+      ? inspect(JSON.parse(stringifiedMeta), { colors: options.colors, depth: null })
+      : stringifiedMeta;
+
+    return (
+      `${color(`[${appName}]`)} ` +
+      `${yellow(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
+      ("undefined" !== typeof timestamp ? `${timestamp} ` : "") +
+      ("undefined" !== typeof context ? `${yellow("[" + context + "]")} ` : "") +
+      `${color(message)} - ` +
+      `${formattedMeta}` +
+      ("undefined" !== typeof ms ? ` ${yellow(ms)}` : "")
+    );
+  });
 
 /**
  * A 'WinstonModule' used for logging only to Console.
