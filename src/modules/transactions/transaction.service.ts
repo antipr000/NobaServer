7dc6b4d1ca9d22b62cdd24b1811c465d8727838c
Soham Mukherjee <soham@noba.com>
2022-11-11 21:28:43 +0530

    Added networkType validation logic in isValidDestinationAddress

--- src/modules/transactions/transaction.service.ts
+++ src/modules/transactions/transaction.service.ts
@@ -3,7 +3,7 @@ import { validate } from "multicoin-address-validator";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston";
 import { NobaConfigs, NobaTransactionConfigs } from "../../config/configtypes/NobaConfigs";
-import { NOBA_CONFIG_KEY } from "../../config/ConfigurationUtils";
+import { AppEnvironment, getEnvironmentName, NOBA_CONFIG_KEY } from "../../config/ConfigurationUtils";
 import { PaginatedResult } from "../../core/infra/PaginationTypes";
 import { CustomConfigService } from "../../core/utils/AppConfigModule";
 import { Utils } from "../../core/utils/Utils";
@@ -574,9 +574,9 @@ export class TransactionService {
     // Strip anything from the first period onward
     const checkCurr = curr.split(".")[0];
 
-    // const networkType = getEnvironmentName() === AppEnvironment.PROD ? "prod" : "testnet";
+    const networkType = getEnvironmentName() === AppEnvironment.PROD ? "prod" : "testnet";
 
-    return validate(destinationWalletAddress, checkCurr);
+    return validate(destinationWalletAddress, checkCurr, networkType);
   }
 
   private isCryptocurrencyAllowed(partner: Partner, cryptocurrency: string) {
