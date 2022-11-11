e912ab38677e1c64a0611fda136477479c8695c1
Soham Mukherjee <soham@noba.com>
2022-11-11 18:40:07 +0530

    Seeded noba devs as partner admins to Noba partner record

--- src/infraproviders/seeders/noba.partner.seed.ts
+++ src/infraproviders/seeders/noba.partner.seed.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { NobaConfigs } from "../../config/configtypes/NobaConfigs";
-import { NOBA_CONFIG_KEY } from "../../config/ConfigurationUtils";
+import { isProductionEnvironment, NOBA_CONFIG_KEY } from "../../config/ConfigurationUtils";
 import { Partner, PartnerProps } from "../../modules/partner/domain/Partner";
 import { CustomConfigService } from "../../core/utils/AppConfigModule";
 import { DBProvider } from "../DBProvider";
 import { convertDBResponseToJsObject } from "../../infra/mongodb/MongoDBUtils";
+import { PartnerAdmin, PARTNER_ADMIN_ROLE_TYPES } from "../../modules/partner/domain/PartnerAdmin";
 
 @Injectable()
 export class NobaPartnerSeed {
@@ -14,6 +15,62 @@ export class NobaPartnerSeed {
   constructor(private readonly dbProvider: DBProvider, configService: CustomConfigService) {
     this.partnerId = configService.get<NobaConfigs>(NOBA_CONFIG_KEY).partnerID;
     this.apiKeyForEmbed = configService.get<NobaConfigs>(NOBA_CONFIG_KEY).apiKeyForEmbed;
+  }
+
+  async seedPartnerAdmin(email: string, name: string) {
+    const partnerAdminModel = await this.dbProvider.getPartnerAdminModel();
+    const partnerAdminRecord = await partnerAdminModel.findOne({ email: email, partnerId: this.partnerId }).exec();
+
+    if (!partnerAdminRecord) {
+      const partnerAdmin = PartnerAdmin.createPartnerAdmin({
+        email: email,
+        name: name,
+        role: PARTNER_ADMIN_ROLE_TYPES.ALL,
+        partnerId: this.partnerId,
+      });
+
+      await partnerAdminModel.create(partnerAdmin.props);
+    }
+  }
+
+  async seedPartnerAdmins() {
+    if (!isProductionEnvironment()) {
+      console.log("Seeding Noba devs as partner admins");
+      const adminsToAdd = [
+        {
+          name: "Soham Mukherjee",
+          email: "soham@noba.com",
+        },
+        {
+          name: "Jonathan Wu",
+          email: "jonathan@noba.com",
+        },
+        {
+          name: "John Nguyen",
+          email: "john@noba.com",
+        },
+        {
+          name: "Ankit Gaur",
+          email: "ankit@noba.com",
+        },
+        {
+          name: "Subham Agarwal",
+          email: "subham@noba.com",
+        },
+        {
+          name: "Mohit Mohta",
+          email: "mohit@noba.com",
+        },
+        {
+          name: "Justin Ashworth",
+          email: "justin@noba.com",
+        },
+      ];
+
+      const promises = adminsToAdd.map(partnerAdmin => this.seedPartnerAdmin(partnerAdmin.email, partnerAdmin.name));
+
+      await Promise.all(promises);
+    }
   }
 
   async seed() {
@@ -34,6 +91,7 @@ export class NobaPartnerSeed {
 
       if (allPartners.filter(partner => this.partnerId === partner.props._id).length > 0) {
         console.log("Noba partner already exists");
+        await this.seedPartnerAdmins();
         return;
       }
     } catch (e) {
@@ -47,5 +105,7 @@ export class NobaPartnerSeed {
     });
 
     await partnerModel.create(partner.props);
+
+    await this.seedPartnerAdmins();
   }
 }
