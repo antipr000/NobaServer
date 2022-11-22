806048e036e54c3bc5b70a6ca5c348cf79973857
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2022-11-22 22:54:38 +0530

    Registers the 'discounts' migrator and reverted the 'discounts' check in repo

--- src/infraproviders/migrators/migrator.service.ts
+++ src/infraproviders/migrators/migrator.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ConsumerMigrator } from "./consumer.migration";
 import { PaymentMethodSchemeMigrator } from "./payment.method.scheme.migration";
+import { TransactionDiscountsMigrator } from "./transaction.discounts.migrator";
 import { TransactionMigrator } from "./transaction.migrator";
 
 @Injectable()
@@ -9,6 +10,7 @@ export class MigratorService {
     private readonly consumerMigrator: ConsumerMigrator,
     private readonly paymentMethodSchemeMigrator: PaymentMethodSchemeMigrator,
     private readonly transactionMigrator: TransactionMigrator,
+    private readonly transactionDiscountsMigrator: TransactionDiscountsMigrator,
   ) {}
 
   // Any error here would lead to server crash. It is intentional!
@@ -21,5 +23,8 @@ export class MigratorService {
 
     // Ensure "Transaction" collection is migrated correctly for Plaid+Checkout Integration
     await this.transactionMigrator.migrate();
+
+    // Ensure 'discounts' sub-collection in "Transaction" collection is migrated correctly.
+    await this.transactionDiscountsMigrator.migrate();
   }
 }
