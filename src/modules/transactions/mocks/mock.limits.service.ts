d8c146af7bbbdcdeea8e1d2e9fbbb589f16c8828
Soham Mukherjee <soham@noba.com>
2022-11-29 23:06:09 +0530

    Fixed failing tests

--- src/modules/transactions/mocks/mock.limits.service.ts
+++ src/modules/transactions/mocks/mock.limits.service.ts
@@ -4,8 +4,12 @@ import { LimitsService } from "../limits.service";
 export function getMockLimitsServiceWithDefaults(): LimitsService {
   const limitsService = mock(LimitsService);
 
-  when(limitsService.canMakeTransaction(anything(), anyNumber(), anything())).thenReject(new Error("Not implemented!"));
-  when(limitsService.getConsumerLimits(anything(), anything())).thenReject(new Error("Not implemented!"));
+  when(limitsService.canMakeTransaction(anything(), anyNumber(), anyString(), anyString(), anyString())).thenReject(
+    new Error("Not implemented!"),
+  );
+  when(limitsService.getConsumerLimits(anything(), anyString(), anyString(), anyString())).thenReject(
+    new Error("Not implemented!"),
+  );
   when(limitsService.getLimits(anything(), anyString(), anyString())).thenReject(new Error("Not implemented!"));
   return limitsService;
 }
