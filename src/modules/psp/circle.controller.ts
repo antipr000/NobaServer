cfb37cce5e13117f42a18e5749e57d4ea91941e3
Jonathan Wu <jonathanwu70@gmail.com>
2022-12-13 14:07:40 -0500

    add stub for wallet balance controller

--- src/modules/psp/circle.controller.ts
+++ src/modules/psp/circle.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ForbiddenException, Headers, HttpStatus, Inject, Post, Request } from "@nestjs/common";
+import { Body, Controller, ForbiddenException, Get, Headers, HttpStatus, Inject, Post, Request } from "@nestjs/common";
 import { ApiBearerAuth, ApiHeaders, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { getCommonHeaders } from "src/core/utils/CommonHeaders";
@@ -21,11 +21,25 @@ export class CircleController {
   ) {
 	  }
 
-  @Post("/wallets")
-  @ApiTags("Wallets")
-  @ApiOperation({ summary: "Checks if the transaction parameters are valid" })
+  @Post("/wallet")
+  @ApiTags("Wallet")
+  @ApiOperation({ summary: "Add circle wallet to current consumer" })
   @ApiResponse({ status: HttpStatus.CREATED })
   async addConsumerWallet(@Request() request, @Headers() headers) {
+    const consumer = request.user.entity;
+    if (!(consumer instanceof Consumer)) {
+      throw new ForbiddenException("Endpoint can only be called by consumers");
+    }
+
+    const res = await this.circleService.createWallet(consumer.props._id);
+    return res;
+  }
+
+	@Get('/wallet/balance')
+	@ApiTags("Wallet")
+	@ApiOperation({ summary: "Get current consumer's circle wallet balance" })
+  @ApiResponse({ status: HttpStatus.OK })
+  async getConsumerWalletBalance(@Request() request, @Headers() headers) {
     const consumer = request.user.entity;
     if (!(consumer instanceof Consumer)) {
       throw new ForbiddenException("Endpoint can only be called by consumers");
