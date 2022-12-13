cefa1ec8649614eedb41dbd284e1040140c0ee6c
Jonathan Wu <jonathanwu70@gmail.com>
2022-12-13 02:28:57 -0500

    add more comments around repo

--- src/modules/psp/repos/CircleRepo.ts
+++ src/modules/psp/repos/CircleRepo.ts
@@ -2,6 +2,6 @@ import { Result } from "src/core/logic/Result";
 
 // Circle repo stub
 export interface ICircleRepo {
-  addConsumerCircleWalletID(consumerID: string, circleWalletID: string): Promise<void>;
-	getWallet(consumerID): Promise<Result<string>>; // guessing result here to encapsulate more state
+  addConsumerCircleWalletID(consumerID: string, circleWalletID: string): Promise<void>; // may need failure state/return
+	getWallet(consumerID): Promise<Result<string>>; // guessing result return object here to encapsulate more state
 }
