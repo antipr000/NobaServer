291588c22eba5985b67ca3d3107f4aab86b55a1c
Jonathan Wu <jonathan@noba.com>
2022-12-22 01:26:21 -0500

    stub out circle service tests

---
+++ src/modules/psp/__tests__/circle.service.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { TestConfigModule } from "src/core/utils/AppConfigModule";
+import { getTestWinstonModule } from "src/core/utils/WinstonModule";
+import { instance } from "ts-mockito";
+import { CircleClient } from "../circle.client";
+import { CircleService } from "../circle.service";
+import { getMockCircleClientWithDefaults } from "../mocks/mock.circle.client";
+import { getMockCircleServiceWithDefaults } from "../mocks/mock.circle.service";
+
+describe("CircleService", () => {
+  let circleService: CircleService;
+  let circleClient: CircleClient;
+
+  jest.setTimeout(10000);
+
+  beforeEach(async () => {
+    circleClient = getMockCircleClientWithDefaults();
+
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [TestConfigModule.registerAsync({}), getTestWinstonModule()],
+      controllers: [],
+      providers: [
+        {
+          provide: CircleClient,
+          useFactory: () => instance(circleClient),
+        },
+        CircleService,
+      ],
+    }).compile();
+
+    circleService = app.get<CircleService>(CircleService);
+  });
+});
