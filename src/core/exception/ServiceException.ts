025b811880f2a5bb3197cd8966f83260ddcfab0f
Jonathan Wu <jonathanwu70@gmail.com>
2022-12-22 20:25:33 -0500

    upgrade to TS 4.9 to use accessors

--- src/core/exception/ServiceException.ts
+++ src/core/exception/ServiceException.ts
@@ -1,21 +1,16 @@
-const SERVICE_ERROR_CODE = {
-  EMAIL_ALREADY_EXISTS: "EmailAlreadyExists",
-  ITEM_NOT_FOUND_IN_DB: "ItemDoesNotExistsInDB",
-  DUPLICATE_ITEM_EXCEPTION: "DuplicateItemsException",
-  INVALID_INPUT: "InvalidInput",
-} as const;
+import { GenericException } from "./GenericException";
 
-type ServiceErrorCode = keyof typeof SERVICE_ERROR_CODE;
+export const enum ServiceErrorCode {
+  SEMANTIC_VALIDATION = "Semantic validation error",
+  DOES_NOT_EXIST = "Item does not exist error",
+  UNAUTHORIZED = "Unauthorized error",
+  UNAUTHENTICATED = "Unauthenticated error",
+  NOT_IMPLEMENTED = "Not implemented error",
+  UNKNOWN = "Unknown error",
+}
 
-export type ServiceExceptionConstructor = {
-  message?: string; //this is not visible to client, if not set will default to messageForClient
-  errorCode?: ServiceErrorCode;
-  clientExceptionData?: any;
-  error?: any;
-};
-
-export class ServiceException extends Error {
-  constructor(params: ServiceExceptionConstructor) {
-    super(params.message);
+export class ServiceException extends GenericException<ServiceErrorCode> {
+  constructor(message: string, errorCode: ServiceErrorCode, error?: any) {
+    super({ message, errorCode, error });
   }
 }
