245641f024ffb978219761770d53d1e5b35427b8
Jonathan Wu <jonathanwu70@gmail.com>
2022-12-22 21:20:10 -0500

    service to HTTP conversion

---
+++ src/core/exception/mappers/serviceToHTTP.ts
@@ -0,0 +1,29 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  HttpException,
+  InternalServerErrorException,
+  NotFoundException,
+  NotImplementedException,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { ServiceErrorCode, ServiceException } from "../ServiceException";
+
+export function serviceToHTTP(exception: ServiceException) {
+  switch (exception.errorCode) {
+    case ServiceErrorCode.DOES_NOT_EXIST:
+      return new NotFoundException(exception.message);
+    case ServiceErrorCode.NOT_IMPLEMENTED:
+      return new NotImplementedException(exception.message);
+    case ServiceErrorCode.SEMANTIC_VALIDATION:
+      return new BadRequestException(exception.message);
+    case ServiceErrorCode.UNAUTHENTICATED:
+      return new UnauthorizedException(exception.message); // The semantic meaning of 401 is no credentials
+    case ServiceErrorCode.UNAUTHORIZED:
+      return new ForbiddenException(exception.message); // The semantic meaning of 403 is incorrect rights
+    case ServiceErrorCode.UNKNOWN:
+      return new InternalServerErrorException(exception.message);
+    default:
+      return new HttpException(exception.message, 500);
+  }
+}
