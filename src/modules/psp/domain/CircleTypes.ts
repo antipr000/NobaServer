bc73af8cd1fb063d544b36d5af6a3b4f439795ca
Jonathan Wu <jonathan@noba.com>
2022-12-26 01:23:45 -0500

    more exception handling work

--- src/modules/psp/domain/CircleTypes.ts
+++ src/modules/psp/domain/CircleTypes.ts
@@ -13,6 +13,7 @@ export interface CircleWithdrawalResponse {
 
 export enum CircleWithdrawalStatus {
   SUCCESS = "SUCCESS",
+  PENDING = "PENDING",
   INSUFFICIENT_FUNDS = "INSUFFICIENT_FUNDS",
   WALLET_NOT_FOUND = "WALLET_NOT_FOUND",
   INTERNAL_ERROR = "INTERNAL_ERROR", // safe to retry with the same 'idempotencyKey'.
