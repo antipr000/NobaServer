8128da15ca1a2e6b2c919ff93c07eeee18c3bdce
Jonathan Wu <jonathan@noba.com>
2022-12-27 12:30:38 -0500

    add severity level

--- src/core/exception/BaseException.ts
+++ src/core/exception/BaseException.ts
@@ -1,16 +1,30 @@
 export type BaseExceptionConstructor<T> = {
   message: string;
   errorCode?: T;
+  severity?: SeverityLevel;
   error?: any;
 };
 
+export type SeverityLevel = "HIGH" | "MEDIUM" | "LOW";
+
 export class BaseException<T> extends Error {
   accessor errorCode: T;
+  accessor severity: SeverityLevel = "LOW";
   accessor error: any;
 
   constructor(params: BaseExceptionConstructor<T>) {
     super(params.message);
     this.errorCode = params.errorCode;
+    this.severity = params.severity;
     this.error = params.error;
+  }
+
+  public generateLogMessage() {
+    return JSON.stringify({
+      message: this.message,
+      errorCode: this.errorCode,
+      severity: this.severity,
+      error: this.error,
+    });
   }
 }
