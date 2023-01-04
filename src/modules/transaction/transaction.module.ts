2a904641317a945119e4a55c3e0ab4a2f04e4f51
Soham Mukherjee <soham@noba.com>
2023-01-04 21:15:14 +0530

    Added workflow executor dependency to transaction service

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -4,9 +4,10 @@ import { TransactionRepoModule } from "./repo/transaction.repo.module";
 import { TransactionController } from "./transaction.controller";
 import { TransactionService } from "./transaction.service";
 import { ConsumerModule } from "../consumer/consumer.module";
+import { TemporalModule } from "../../infra/temporal/temporal.module";
 
 @Module({
-  imports: [InfraProvidersModule, TransactionRepoModule, ConsumerModule],
+  imports: [InfraProvidersModule, TransactionRepoModule, ConsumerModule, TemporalModule],
   controllers: [TransactionController],
   providers: [TransactionService],
   exports: [TransactionService], //Need to access in PublicController
