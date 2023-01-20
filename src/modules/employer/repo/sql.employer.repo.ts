558aa10cbebce7f9dfb588737cff36e55f84dd06
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2023-01-20 18:46:22 +0530

    Fixed a code smell reported by Sonar

--- src/modules/employer/repo/sql.employer.repo.ts
+++ src/modules/employer/repo/sql.employer.repo.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { Employer } from "@prisma/client";
 import {
   convertToDomainEmployer,
+  Employer,
   EmployerCreateRequest,
   EmployerUpdateRequest,
   validateCreateEmployerRequest,
