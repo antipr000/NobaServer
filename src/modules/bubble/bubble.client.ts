45f46d075f35ab16f3daca486b710f286bbb5cb5
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2023-01-20 19:09:00 +0530

    Added logic to call Bubble APIs for Employees

--- src/modules/bubble/bubble.client.ts
+++ src/modules/bubble/bubble.client.ts
@@ -1,9 +1,12 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
-import { BubbleConfigs } from "src/config/configtypes/BubbleConfigs";
-import { BUBBLE_CONFIG_KEY } from "src/config/ConfigurationUtils";
-import { CustomConfigService } from "src/core/utils/AppConfigModule";
+import { BubbleConfigs } from "../../config/configtypes/BubbleConfigs";
+import { BUBBLE_CONFIG_KEY } from "../../config/ConfigurationUtils";
+import { CustomConfigService } from "../../core/utils/AppConfigModule";
 import { Logger } from "winston";
+import { NewEmployeeRegisterRequest } from "./dto/bubble.client.dto";
+import axios from "axios";
+import { ServiceErrorCode, ServiceException } from "../../core/exception/ServiceException";
 
 @Injectable()
 export class BubbleClient {
@@ -14,5 +17,63 @@ export class BubbleClient {
     const bubbleConfigs: BubbleConfigs = configService.get<BubbleConfigs>(BUBBLE_CONFIG_KEY);
     this.bearerToken = bubbleConfigs.bearerToken;
     this.baseUrl = bubbleConfigs.baseURL;
+  }
+
+  private getAuthorizationHeader(): Record<string, string> {
+    return {
+      Authorization: `Bearer ${this.bearerToken}`,
+    };
+  }
+
+  async registerNewEmployee(request: NewEmployeeRegisterRequest): Promise<void> {
+    this.logger.info(`Registering new employee: ${JSON.stringify(request)} to Bubble`);
+
+    const url = `${this.baseUrl}/employees`;
+    const headers = {
+      ...this.getAuthorizationHeader(),
+    };
+    const requestBody = {
+      first_name: request.firstName,
+      last_name: request.lastName,
+      email: request.email,
+      phone: request.phone,
+      employer_id: request.employerID,
+      allocation_amount_in_pesos: request.allocationAmountInPesos,
+      noba_employee_id: request.nobaEmployeeID,
+    };
+
+    try {
+      await axios.post(url, requestBody, { headers });
+      this.logger.info(`Successfully registered new employee: ${JSON.stringify(request)} to Bubble`);
+    } catch (err) {
+      this.logger.error(`Failed to register new employee: ${JSON.stringify(request)} to Bubble. Error: ${err}`);
+      throw new ServiceException({
+        message: "Error while creating Mono collection link",
+        errorCode: ServiceErrorCode.UNKNOWN,
+      });
+    }
+  }
+
+  async updateEmployeeAllocationAmount(nobaEmployeeID: string, allocationAmountInPesos: number): Promise<void> {
+    this.logger.info(`Updating employee allocation amount: ${nobaEmployeeID} to Bubble`);
+
+    const url = `${this.baseUrl}/employees/${nobaEmployeeID}`;
+    const headers = {
+      ...this.getAuthorizationHeader(),
+    };
+    const requestBody = {
+      allocation_amount_in_pesos: allocationAmountInPesos,
+    };
+
+    try {
+      await axios.patch(url, requestBody, { headers });
+      this.logger.info(`Successfully updated employee allocation amount: ${nobaEmployeeID} to Bubble`);
+    } catch (err) {
+      this.logger.error(`Failed to update employee allocation amount: ${nobaEmployeeID} to Bubble. Error: ${err}`);
+      throw new ServiceException({
+        message: "Error while creating Mono collection link",
+        errorCode: ServiceErrorCode.UNKNOWN,
+      });
+    }
   }
 }
---
+++ src/modules/bubble/dto/bubble.client.dto.ts
@@ -0,0 +1,9 @@
+export type NewEmployeeRegisterRequest = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  employerID: string;
+  allocationAmountInPesos: number;
+  nobaEmployeeID: string;
+};
