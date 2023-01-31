b396f25d6e8e8293effe61fb889533c372b5d8cd
Jonathan Wu <jonathanwu70@gmail.com>
2023-01-31 04:12:33 -0500

    migrations should finally be good

---
+++ prisma/migrations/20230131091214_crypto_485/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "PushToken" (
+    "id" TEXT NOT NULL,
+    "consumerID" TEXT NOT NULL,
+    "pushToken" TEXT NOT NULL,
+    "createdTimestamp" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedTimestamp" TIMESTAMP(3),
+
+    CONSTRAINT "PushToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PushToken_pushToken_key" ON "PushToken"("pushToken");
+
+-- CreateIndex
+CREATE INDEX "PushToken_consumerID_pushToken_idx" ON "PushToken"("consumerID", "pushToken");
+
+-- AddForeignKey
+ALTER TABLE "PushToken" ADD CONSTRAINT "PushToken_consumerID_fkey" FOREIGN KEY ("consumerID") REFERENCES "Consumer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
