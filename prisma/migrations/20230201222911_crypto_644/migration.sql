4ee10ef3e51f0076234da319c6f933c9d0d70944
Jonathan Wu <jonathanwu70@gmail.com>
2023-02-01 17:31:07 -0500

    db changes

---
+++ prisma/migrations/20230201222911_crypto_644/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `payrollDays` on the `Employer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Employer" DROP COLUMN "payrollDays",
+ADD COLUMN     "payrollDates" TIMESTAMP(3)[];
