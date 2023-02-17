ffbd3db3798d1157c1ff8e76cdd7d6850fb85ed4
Soham Mukherjee <soham@noba.com>
2023-02-17 20:21:32 +0530

    Added IsNoApiKeyNeeded decorator and made api key and secret mandatory for e2e test to detect the failures of kyc notifications

--- src/core/utils/CommonHeaders.ts
+++ src/core/utils/CommonHeaders.ts
@@ -1,6 +1,6 @@
 import { ApiHeaderOptions } from "@nestjs/swagger";
 import { X_NOBA_API_KEY, X_NOBA_SIGNATURE, X_NOBA_TIMESTAMP } from "../../modules/auth/domain/HeaderConstants";
-import { isProductionEnvironment } from "../../config/ConfigurationUtils";
+import { isE2ETestEnvironment, isProductionEnvironment } from "../../config/ConfigurationUtils";
 
 export function getCommonHeaders(): ApiHeaderOptions[] {
   return [
@@ -10,11 +10,11 @@ export function getCommonHeaders(): ApiHeaderOptions[] {
     },
     {
       name: X_NOBA_SIGNATURE,
-      required: isProductionEnvironment(),
+      required: isProductionEnvironment() || isE2ETestEnvironment(),
     },
     {
       name: X_NOBA_TIMESTAMP,
-      required: isProductionEnvironment(),
+      required: isProductionEnvironment() || isE2ETestEnvironment(),
       description: "Timestamp in milliseconds, use: new Date().getTime().toString()",
     },
   ];
