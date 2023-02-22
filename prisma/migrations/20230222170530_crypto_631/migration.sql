0ccc0459caa7ad29fd0b012853e3fa69e810d131
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2023-02-22 22:42:27 +0530

    Deprecated the unused column

---
+++ prisma/migrations/20230222170530_crypto_631/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `monoTransactionID` on the `Mono` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Mono" DROP COLUMN "monoTransactionID";
