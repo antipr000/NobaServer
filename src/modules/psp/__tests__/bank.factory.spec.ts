b564e08d97fd5faa5c3b7c25a7cf835aa531db61
Jonathan Wu <jonathanwu70@gmail.com>
2023-02-28 12:26:40 -0500

    fix test

--- src/modules/psp/__tests__/bank.factory.spec.ts
+++ src/modules/psp/__tests__/bank.factory.spec.ts
@@ -4,20 +4,21 @@ import { TestConfigModule } from "../../../core/utils/AppConfigModule";
 import { getTestWinstonModule } from "../../../core/utils/WinstonModule";
 import { instance } from "ts-mockito";
 import { BankFactory } from "../factory/bank.factory";
-import { BankMonoImpl } from "../factory/bank.mono.impl";
 import { getMockBankMonoImplWithDefaults } from "../mocks/mock.bank.mono.impl";
 import { BankName } from "../domain/BankFactoryTypes";
 import { ServiceException } from "../../../core/exception/service.exception";
+import { MonoWorkflowService } from "../mono/mono.workflow.service";
+import { getMockMonoWorkflowServiceWithDefaults } from "../mono/mocks/mock.mono.workflow.service";
 
 describe("BankFactory Tests", () => {
   jest.setTimeout(20000);
 
   let app: TestingModule;
   let bankFactory: BankFactory;
-  let bankMonoImpl: BankMonoImpl;
+  let monoWorkflowService: MonoWorkflowService;
 
   beforeAll(async () => {
-    bankMonoImpl = instance(getMockBankMonoImplWithDefaults());
+    monoWorkflowService = instance(getMockMonoWorkflowServiceWithDefaults());
     const appConfigurations = {
       [SERVER_LOG_FILE_PATH]: `/tmp/test-${Math.floor(Math.random() * 1000000)}.log`,
     };
@@ -27,8 +28,8 @@ describe("BankFactory Tests", () => {
       imports: [TestConfigModule.registerAsync(appConfigurations), getTestWinstonModule()],
       providers: [
         {
-          provide: BankMonoImpl,
-          useFactory: () => bankMonoImpl,
+          provide: MonoWorkflowService,
+          useFactory: () => monoWorkflowService,
         },
         BankFactory,
       ],
@@ -44,7 +45,7 @@ describe("BankFactory Tests", () => {
   describe("getBankImplementationByCurrency", () => {
     it("should return BankMonoImpl when currency is COP", () => {
       const workflow = bankFactory.getBankImplementationByCurrency("COP");
-      expect(workflow).toBe(bankMonoImpl);
+      expect(workflow).toBe(monoWorkflowService);
     });
   });
 });
