8bf398167f45065472efac68273675e4f1058eb0
Jonathan Wu <jonathanwu70@gmail.com>
2023-02-28 13:17:45 -0500

    module resolution

--- src/modules/psp/factory/bank.factory.module.ts
+++ src/modules/psp/factory/bank.factory.module.ts
@@ -5,11 +5,10 @@ import { BankFactory } from "./bank.factory";
 import { CircleService } from "../circle.service";
 import { CircleRepoModule } from "../repos/circle.repo.module";
 import { CircleClient } from "../circle.client";
-import { MonoWorkflowService } from "../mono/mono.workflow.service";
 
 @Module({
-  imports: [MonoModule, CommonModule, CircleRepoModule],
-  providers: [BankFactory, MonoWorkflowService, CircleService, CircleClient],
+  imports: [MonoModule, CommonModule, CircleRepoModule, MonoModule],
+  providers: [BankFactory, CircleService, CircleClient],
   exports: [BankFactory],
 })
 export class BankFactoryModule {}
