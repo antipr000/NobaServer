6af58ca6367594a69c25b3f7fd8757e7d72b4282
Jonathan Wu <jonathanwu70@gmail.com>
2023-03-03 13:37:06 -0500

    initial push, need to refactor service

--- src/modules/common/handlebar.service.ts
+++ src/modules/common/handlebar.service.ts
@@ -1,9 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { S3, GetObjectCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
-import { parse } from "csv";
+import { S3, GetObjectCommand, ListObjectsV2Command, PutObjectCommand } from "@aws-sdk/client-s3";
 import { ASSETS_BUCKET_NAME, TEMPLATES_FOLDER_BUCKET_PATH } from "../../config/ConfigurationUtils";
 import { CustomConfigService } from "../../core/utils/AppConfigModule";
-import { Readable } from "stream";
 
 @Injectable()
 export class TemplateService {
@@ -29,10 +27,37 @@ export class TemplateService {
     });
   }
 
+  private async pushHTMLToS3(folderPath: string, objectName: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const s3 = new S3({});
+      const options = {
+        Bucket: this.configService.get(ASSETS_BUCKET_NAME),
+        Key: this.configService.get(TEMPLATES_FOLDER_BUCKET_PATH) + folderPath + objectName,
+      };
+
+      try {
+        const putObjectCommand = new PutObjectCommand(options);
+        const putObjectResult = await s3.send(putObjectCommand);
+        resolve(putObjectResult);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
   public async getHandlebarLanguageTemplates(): Promise<Record<string, string>> {
     const [handlebarEnglishTemplate, handlebarSpanishTemplate] = await Promise.all([
       this.loadTemplatesFromS3("/payroll-invoice/", "template_en.hbs"),
       this.loadTemplatesFromS3("/payroll-invoice/", "template_es.hbs"),
+    ]);
+
+    return { en: handlebarEnglishTemplate, es: handlebarSpanishTemplate };
+  }
+
+  public async pushHandlebarLanguageHTML(): Promise<Record<string, string>> {
+    const [handlebarEnglishTemplate, handlebarSpanishTemplate] = await Promise.all([
+      this.pushHTMLToS3("/payroll-invoice/", "template_en.hbs"),
+      this.pushHTMLToS3("/payroll-invoice/", "template_es.hbs"),
     ]);
 
     return { en: handlebarEnglishTemplate, es: handlebarSpanishTemplate };
