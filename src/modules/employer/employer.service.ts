259017498ec73af0b45a5de6b215735457d7a57c
Jonathan Wu <jonathanwu70@gmail.com>
2023-03-08 17:04:44 -0500

    remove logs

--- src/modules/employer/employer.service.ts
+++ src/modules/employer/employer.service.ts
@@ -183,8 +183,6 @@ export class EmployerService {
       });
     }
 
-    console.log(payrollID);
-
     const templatesPromise = Promise.all([
       this.handlebarService.getHandlebarLanguageTemplate(`template_${this.ENGLISH_LOCALE}.hbs`),
       this.handlebarService.getHandlebarLanguageTemplate(`template_${this.SPANISH_LOCALE}.hbs`),
@@ -208,8 +206,6 @@ export class EmployerService {
         message: "Employer not found",
       });
     }
-
-    console.log(employer);
 
     const companyName = employer.name;
     const currency = payroll.debitCurrency;
