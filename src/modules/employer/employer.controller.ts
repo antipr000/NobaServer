335e2ed1668ebf8c77fde2876f85d5f288d301a4
Justin-noba <justin@noba.com>
2023-03-08 08:58:13 -0800

    Removed API header requirements for get Employer by referral id and removed unnecessary testing endpoint

--- src/modules/employer/employer.controller.ts
+++ src/modules/employer/employer.controller.ts
@@ -1,18 +1,16 @@
-import { Controller, HttpStatus, Inject, NotFoundException, Param, Get, Post, Body, Query } from "@nestjs/common";
-import { ApiBearerAuth, ApiHeaders, ApiNotFoundResponse, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Controller, HttpStatus, Inject, NotFoundException, Param, Get } from "@nestjs/common";
+import { ApiNotFoundResponse, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
-import { getCommonHeaders } from "../../core/utils/CommonHeaders";
 import { Logger } from "winston";
 import { Role } from "../auth/role.enum";
 import { Roles } from "../auth/roles.decorator";
 import { EmployerService } from "./employer.service";
-import { EmployerDTO, PayrollData } from "./dto/employer.controller.dto";
+import { EmployerDTO } from "./dto/employer.controller.dto";
 import { EmployerMapper } from "./mappers/employer.mapper";
+import { IsNoApiKeyNeeded } from "../auth/public.decorator";
 
 @Controller("v1/employers")
 @Roles(Role.CONSUMER)
-@ApiBearerAuth("JWT-auth")
-@ApiHeaders(getCommonHeaders())
 @ApiTags("Employers")
 export class EmployerController {
   @Inject(WINSTON_MODULE_PROVIDER)
@@ -28,6 +26,7 @@ export class EmployerController {
   }
 
   @Get("/:referralID")
+  @IsNoApiKeyNeeded()
   @ApiOperation({ summary: "Retrieve employer details by referral ID" })
   @ApiResponse({
     status: HttpStatus.OK,
@@ -42,17 +41,5 @@ export class EmployerController {
     }
 
     return this.employerMapper.toEmployerDTO(employer);
-  }
-
-  @Post("/payroll")
-  @ApiBearerAuth()
-  @ApiOperation({ summary: "Generate payroll for employer" })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: "Payroll Generated",
-  })
-  @ApiNotFoundResponse({ description: "Employer not found" })
-  async generatePayroll(@Query("payrollID") payrollID: string): Promise<void> {
-    this.employerService.generatePayroll(payrollID);
   }
 }
