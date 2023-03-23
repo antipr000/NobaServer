53884048f887c9d3d7a8671f707ea059d1b21878
Soham Mukherjee <soham@noba.com>
2023-03-23 20:06:01 +0530

    Removing redundant indexes

---
+++ prisma/migrations/20230323143512_crypto_898/migration.sql
@@ -0,0 +1,41 @@
+-- DropIndex
+DROP INDEX "Admin_email_idx";
+
+-- DropIndex
+DROP INDEX "Circle_consumerID_idx";
+
+-- DropIndex
+DROP INDEX "Consumer_email_idx";
+
+-- DropIndex
+DROP INDEX "Consumer_handle_idx";
+
+-- DropIndex
+DROP INDEX "Consumer_phone_idx";
+
+-- DropIndex
+DROP INDEX "Consumer_referralCode_idx";
+
+-- DropIndex
+DROP INDEX "CreditCardBIN_bin_idx";
+
+-- DropIndex
+DROP INDEX "CryptoWallet_address_idx";
+
+-- DropIndex
+DROP INDEX "Employee_consumerID_idx";
+
+-- DropIndex
+DROP INDEX "Employer_referralID_idx";
+
+-- DropIndex
+DROP INDEX "Mono_nobaTransactionID_idx";
+
+-- DropIndex
+DROP INDEX "Otp_otpIdentifier_identityType_idx";
+
+-- DropIndex
+DROP INDEX "PaymentMethod_paymentToken_idx";
+
+-- DropIndex
+DROP INDEX "WithdrawalDetails_transactionID_idx";
