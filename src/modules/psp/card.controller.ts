1ed5387f63fce03b014af0ff92a6ce2f7924c7e6
Soham Mukherjee <soham@noba.com>
2023-03-28 10:47:42 +0530

    Made method static in mapper

--- src/modules/psp/card.controller.ts
+++ src/modules/psp/card.controller.ts
@@ -17,14 +17,10 @@ import { CardMapper } from "./mapper/card.mapper";
 @ApiTags("Consumer") // This determines where it shows up in the swagger docs. Seems fair for this to appear in the Consumer grouping.
 @ApiHeaders(getCommonHeaders()) // Adds the requirement for all the X-Noba-xxx headers.
 export class CardController {
-  private readonly cardMapper: CardMapper;
-
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
     private readonly cardService: CardService,
-  ) {
-    this.cardMapper = new CardMapper();
-  }
+  ) {}
 
   @Post("/")
   @ApiOperation({ summary: "Create a new card for the consumer" })
@@ -32,7 +28,7 @@ export class CardController {
   @ApiForbiddenResponse({ description: "Logged-in user is not a Consumer" })
   async createCard(@AuthUser() consumer: Consumer, @Body() request: CardCreateRequestDTO): Promise<CardResponseDTO> {
     const card = await this.cardService.createCard(consumer.props.id, request.type);
-    return this.cardMapper.toCardResponseDTO(card);
+    return CardMapper.toCardResponseDTO(card);
   }
 
   @Get("/")
@@ -41,7 +37,7 @@ export class CardController {
   @ApiForbiddenResponse({ description: "Logged-in user is not a Consumer" })
   async getAllCardsForConsumer(@AuthUser() consumer: Consumer): Promise<CardResponseDTO[]> {
     const cards = await this.cardService.getAllCardsForConsumer(consumer.props.id);
-    return cards.map(card => this.cardMapper.toCardResponseDTO(card));
+    return cards.map(card => CardMapper.toCardResponseDTO(card));
   }
 
   @Get("/:id/token")
@@ -58,6 +54,6 @@ export class CardController {
   @ApiForbiddenResponse({ description: "Logged-in user is not a Consumer" })
   async getCard(@AuthUser() consumer: Consumer, @Param("id") cardID: string): Promise<CardResponseDTO> {
     const card = await this.cardService.getCard(consumer.props.id, cardID);
-    return this.cardMapper.toCardResponseDTO(card);
+    return CardMapper.toCardResponseDTO(card);
   }
 }
--- src/modules/psp/mapper/card.mapper.ts
+++ src/modules/psp/mapper/card.mapper.ts
@@ -2,7 +2,7 @@ import { NobaCard } from "../card/domain/NobaCard";
 import { CardResponseDTO } from "../dto/card.controller.dto";
 
 export class CardMapper {
-  public toCardResponseDTO(card: NobaCard): CardResponseDTO {
+  public static toCardResponseDTO(card: NobaCard): CardResponseDTO {
     return {
       id: card.id,
       lastFourDigits: card.last4Digits,
