9ec783acef059a408553257a57798370d2a0e05f
Jonathan Wu <jonathan@noba.com>
2023-04-22 19:19:24 -0400

    existing test works

--- src/modules/exchangerate/__tests__/exchangerate.factory.spec.ts
+++ src/modules/exchangerate/__tests__/exchangerate.factory.spec.ts
@@ -2,26 +2,25 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { SERVER_LOG_FILE_PATH } from "../../../config/ConfigurationUtils";
 import { TestConfigModule } from "../../../core/utils/AppConfigModule";
 import { getTestWinstonModule } from "../../../core/utils/WinstonModule";
-import { instance, when } from "ts-mockito";
-import { ExchangeRateService } from "../exchangerate.service";
-import { ExchangeRateWorkflowController } from "../workflow/exchangerate.workflow.controller";
-import { getMockExchangeRateServiceWithDefaults } from "../mocks/mock.exchangerate.service";
+import { instance } from "ts-mockito";
 import { ExchangeRateClientFactory } from "../factory/exchangerate.factory";
 import { StubExchangeRateClient } from "../clients/stub.exchangerate.client";
 import { ExchangeRateIOExchangeRateClient } from "../clients/exchangerateio.exchangerate.client";
 import { getMockExchangeRateIOExchangeRateClientWithDefaults } from "../mocks/mock.exchangerateio.exchangerate.client";
+import { ExchangeRateName } from "../domain/ExchangeRate";
+import { IExchangeRateClient } from "../clients/exchangerate.client";
 
 describe("ExchangeRateFactoryTests", () => {
   jest.setTimeout(20000);
 
   let app: TestingModule;
-  let stubClient: StubExchangeRateClient;
+  let stubClient: IExchangeRateClient;
   let exchangeRateFactory: ExchangeRateClientFactory;
-  let mockExchangeRateIOClient: ExchangeRateIOExchangeRateClient;
+  let mockExchangeRateIOClient: IExchangeRateClient;
 
   beforeEach(async () => {
-    stubClient = new StubExchangeRateClient();
-    mockExchangeRateIOClient = getMockExchangeRateIOExchangeRateClientWithDefaults();
+    stubClient = instance(new StubExchangeRateClient());
+    mockExchangeRateIOClient = instance(getMockExchangeRateIOExchangeRateClientWithDefaults());
 
     const appConfigurations = {
       [SERVER_LOG_FILE_PATH]: `/tmp/test-${Math.floor(Math.random() * 1000000)}.log`,
@@ -32,8 +31,12 @@ describe("ExchangeRateFactoryTests", () => {
       imports: [TestConfigModule.registerAsync(appConfigurations), getTestWinstonModule()],
       providers: [
         {
-          provide: ExchangeRateService,
-          useFactory: () => instance(mockExchangeRateService),
+          provide: ExchangeRateIOExchangeRateClient,
+          useFactory: () => mockExchangeRateIOClient,
+        },
+        {
+          provide: StubExchangeRateClient,
+          useFactory: () => stubClient,
         },
         ExchangeRateClientFactory,
       ],
@@ -43,39 +46,30 @@ describe("ExchangeRateFactoryTests", () => {
   });
 
   afterEach(async () => {
-    jest.useRealTimers();
-    app.close();
+    await app.close();
   });
 
-  describe("createExchangeRate", () => {
-    it("should return a new exchange rate", async () => {
-      when(mockExchangeRateService.createExchangeRateFromProvider()).thenResolve([
-        {
-          numeratorCurrency: "USD",
-          denominatorCurrency: "EUR",
-          bankRate: 0.8,
-        },
-      ]);
-
-      const result = await exchangeRateWorkflowController.createExchangeRate();
-
-      expect(result).toEqual([
-        {
-          numeratorCurrency: "USD",
-          denominatorCurrency: "EUR",
-          bankRate: 0.8,
-        },
-      ]);
+  describe("getExchangeRateClient", () => {
+    it("should return a StubExchangeRateClient", async () => {
+      const client = exchangeRateFactory.getExchangeRateClient(ExchangeRateName.STUB);
+      expect(client).toBe(stubClient);
     });
 
-    it("should return an error if the exchange rate cannot be created", async () => {
-      when(mockExchangeRateService.createExchangeRateFromProvider()).thenReject(
-        new Error("Cannot create exchange rate"),
-      );
+    it("should return a ExchangeRateIOExchangeRateClient", async () => {
+      const client = exchangeRateFactory.getExchangeRateClient(ExchangeRateName.EXCHANGERATEIO);
+      expect(client).toBe(mockExchangeRateIOClient);
+    });
+  });
 
-      await expect(exchangeRateWorkflowController.createExchangeRate()).rejects.toThrowError(
-        "Cannot create exchange rate",
-      );
+  describe("getExchangeRateClientByCurrencyPair", () => {
+    it("should return a StubExchangeRateClient", async () => {
+      const client = exchangeRateFactory.getExchangeRateClientByCurrencyPair("COP", "USD");
+      expect(client).toBe(mockExchangeRateIOClient);
+    });
+
+    it("should return a ExchangeRateIOExchangeRateClient", async () => {
+      const client = exchangeRateFactory.getExchangeRateClientByCurrencyPair("USD", "COP");
+      expect(client).toBe(mockExchangeRateIOClient);
     });
   });
 });
