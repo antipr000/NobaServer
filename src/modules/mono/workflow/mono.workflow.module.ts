d3d0510dd4fba1c235f258ccc37a357a796a9670
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2023-04-29 00:34:45 +0530

    Removed MonoWorkflowService

--- src/modules/mono/workflow/mono.workflow.module.ts
+++ src/modules/mono/workflow/mono.workflow.module.ts
@@ -3,7 +3,6 @@ import { ConsumerModule } from "../../consumer/consumer.module";
 import { InfraProvidersModule } from "../../../infraproviders/infra.module";
 import { CommonModule } from "../../common/common.module";
 import { MonoPublicModule } from "../public/mono.public.module";
-import { MonoWorkflowService } from "./mono.workflow.service";
 import { MonoWorkflowController } from "./mono.workflow.controller";
 import { MonoWorkflowControllerMappers } from "./mono.workflow.controller.mappers";
 import { MonoRepoModule } from "../repo/mono.repo.module";
@@ -11,7 +10,7 @@ import { MonoRepoModule } from "../repo/mono.repo.module";
 @Module({
   imports: [InfraProvidersModule, ConsumerModule, CommonModule, MonoPublicModule, MonoRepoModule],
   controllers: [MonoWorkflowController],
-  providers: [MonoWorkflowService, MonoWorkflowControllerMappers],
+  providers: [MonoWorkflowControllerMappers],
   exports: [],
 })
 export class MonoWorkflowModule {}
