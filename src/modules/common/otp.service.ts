851571d4d55018bd634fc98f4013a72cb716698c
Jonathan Wu <jonathanwu70@gmail.com>
2023-05-31 11:16:59 -0400

    add strip spaces back

--- src/modules/common/otp.service.ts
+++ src/modules/common/otp.service.ts
@@ -10,7 +10,10 @@ export class OTPService {
   private readonly otpRepo: IOTPRepo;
 
   async checkIfOTPIsValidAndCleanup(otpIdentifier: string, identityType: IdentityType, otp: number): Promise<boolean> {
-    const otpRecord = await this.otpRepo.getOTP(Utils.stripNonPhoneChars(otpIdentifier), identityType);
+    const otpStrippedRecord = Utils.isEmail(otpIdentifier)
+      ? Utils.stripSpaces(otpIdentifier)
+      : Utils.stripNonPhoneChars(otpIdentifier);
+    const otpRecord = await this.otpRepo.getOTP(otpStrippedRecord, identityType);
     if (!otpRecord || otpRecord.props.otp !== otp || isAfter(new Date(), otpRecord.props.otpExpirationTimestamp)) {
       return false;
     }
@@ -20,8 +23,10 @@ export class OTPService {
   }
 
   async saveOTP(otpIdentifier: string, identityType: IdentityType, otp: number): Promise<void> {
-    // Clean existing otps for identifier if any
-    await this.otpRepo.deleteAllOTPsForIdentifier(Utils.stripNonPhoneChars(otpIdentifier), identityType);
-    await this.otpRepo.saveOTP(Utils.stripNonPhoneChars(otpIdentifier), otp, identityType);
+    const otpRecord = Utils.isEmail(otpIdentifier)
+      ? Utils.stripSpaces(otpIdentifier)
+      : Utils.stripNonPhoneChars(otpIdentifier);
+    await this.otpRepo.deleteAllOTPsForIdentifier(otpRecord, identityType);
+    await this.otpRepo.saveOTP(otpRecord, otp, identityType);
   }
 }
