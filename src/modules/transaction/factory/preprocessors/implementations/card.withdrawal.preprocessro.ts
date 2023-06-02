0c990d0dddcf12baf5235636bfabbed2791f7efc
Subham Agarwal <subhamagarwal@Subhams-MacBook-Air.local>
2023-06-02 15:05:54 +0530

    TransactionPreprocessor interface + few implementations + tests

---
+++ src/modules/transaction/factory/preprocessors/implementations/card.withdrawal.preprocessro.ts
@@ -0,0 +1,58 @@
+import { Injectable } from "@nestjs/common";
+import Joi from "joi";
+import { Utils } from "../../../../../core/utils/Utils";
+import { KeysRequired } from "../../../../../modules/common/domain/Types";
+import { InputTransaction, WorkflowName } from "../../../../../modules/transaction/domain/Transaction";
+import { Currency } from "../../../../../modules/transaction/domain/TransactionTypes";
+import { CardWithdrawalTransactionRequest } from "../../../../../modules/transaction/dto/transaction.service.dto";
+import { TransactionPreprocessor } from "../transaction.preprocessor";
+
+@Injectable()
+export class CardWithdrawalPreprocessor implements TransactionPreprocessor {
+  private readonly validationKeys: KeysRequired<CardWithdrawalTransactionRequest> = {
+    nobaTransactionID: Joi.string().required(),
+    debitConsumerID: Joi.string().required(),
+    debitAmountInUSD: Joi.number().required(),
+    creditAmount: Joi.number().required(),
+    creditCurrency: Joi.string()
+      .required()
+      .valid(...Object.values(Currency)),
+    exchangeRate: Joi.number().required(),
+    memo: Joi.string().required(),
+  };
+
+  constructor() {}
+
+  async validate(request: CardWithdrawalTransactionRequest): Promise<void> {
+    this.performStaticValidations(request);
+    await this.performDynamicValidations(request);
+  }
+
+  async convertToRepoInputTransaction(request: CardWithdrawalTransactionRequest): Promise<InputTransaction> {
+    return {
+      id: request.nobaTransactionID,
+      transactionRef: Utils.generateLowercaseUUID(true),
+      workflowName: WorkflowName.CARD_WITHDRAWAL,
+      debitAmount: request.debitAmountInUSD,
+      debitCurrency: Currency.USD,
+      debitConsumerID: request.debitConsumerID,
+      creditAmount: request.creditAmount,
+      creditCurrency: request.creditCurrency,
+      memo: request.memo,
+      exchangeRate: request.exchangeRate,
+      sessionKey: "CARD_WITHDRAWAL",
+      transactionFees: [],
+    };
+  }
+
+  private performStaticValidations(request: CardWithdrawalTransactionRequest): void {
+    const validationSchema = Joi.object(this.validationKeys).options({
+      allowUnknown: false,
+      stripUnknown: true,
+    });
+
+    Joi.attempt(request, validationSchema);
+  }
+
+  private async performDynamicValidations(request: CardWithdrawalTransactionRequest): Promise<void> {}
+}
---
+++ src/modules/transaction/factory/preprocessors/transaction.preprocessor.ts
@@ -0,0 +1,29 @@
+import { InputTransaction } from "../../domain/Transaction";
+import {
+  CardCreditAdjustmentTransactionRequest,
+  CardDebitAdjustmentTransactionRequest,
+  CardReversalTransactionRequest,
+  CardWithdrawalTransactionRequest,
+  CreditAdjustmentTransactionRequest,
+  DebitAdjustmentTransactionRequest,
+  PayrollDepositTransactionRequest,
+} from "../../dto/transaction.service.dto";
+
+export type TransactionPreprocessorRequest =
+  | PayrollDepositTransactionRequest
+  | CardWithdrawalTransactionRequest
+  | CardReversalTransactionRequest
+  | CreditAdjustmentTransactionRequest
+  | DebitAdjustmentTransactionRequest
+  | CardCreditAdjustmentTransactionRequest
+  | CardDebitAdjustmentTransactionRequest;
+
+export interface TransactionPreprocessor {
+  // Performs static and dynamic validation of the transaction.
+  //   - Will complete successfully if the transaction is valid.
+  //   - Throws an exception if the transaction is invalid.
+  validate(request: TransactionPreprocessorRequest): Promise<void>;
+
+  // Converts the transaction to InputTransaction (interface to the Repository layer).
+  convertToRepoInputTransaction(request: TransactionPreprocessorRequest): Promise<InputTransaction>;
+}
