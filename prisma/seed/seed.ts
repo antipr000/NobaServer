2eda8567647493018919755f12aaca8dfd3d3f17
Soham Mukherjee <soham@noba.com>
2023-06-06 23:44:00 +0530

    Commenting out seeding exchange rates

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { seedAdmins } from "./seed-admins";
-import { seedExchangeRates } from "./seed-exchangerates";
 import { seedConsumers } from "./seed-consumers";
 import { seedEmployers } from "./seed-employers";
 import { seedEventsAndTemplates } from "./seed-events";
@@ -14,7 +13,7 @@ async function main() {
   if (process.env.NODE_ENV !== "production") {
     console.log("Seeding data...");
     await seedAdmins(prisma);
-    await seedExchangeRates(prisma);
+    // await seedExchangeRates(prisma);
     await seedConsumers(prisma);
     await seedEmployers(prisma);
     await seedEmployees(prisma);
