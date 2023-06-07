1e8f53f501ddf691f62a2b059b63adc6fd3245c2
Jonathan Wu <jonathanwu70@gmail.com>
2023-06-07 17:44:35 -0400

    merge fixes

--- src/modules/circle/public/circle.service.ts
+++ src/modules/circle/public/circle.service.ts
@@ -260,11 +260,13 @@ export class CircleService implements IBank {
           await this.circleRepo.updateCurrentBalance(sourceWalletID, Utils.roundTo2DecimalNumber(balance - amount));
         }
 
-        const destinationCurrentBalance = await this.circleClient.getWalletBalance(destinationWalletID);
-        await this.circleRepo.updateCurrentBalance(
-          destinationWalletID,
-          Utils.roundTo2DecimalNumber(destinationCurrentBalance),
-        );
+        if (destinationWalletID !== masterWalletID) {
+          const destinationCurrentBalance = await this.circleClient.getWalletBalance(destinationWalletID);
+          await this.circleRepo.updateCurrentBalance(
+            destinationWalletID,
+            Utils.roundTo2DecimalNumber(destinationCurrentBalance),
+          );
+        }
       } catch (e) {
         this.alertService.raiseAlert({
           key: AlertKey.CIRCLE_BALANCE_UPDATE_FAILED,
